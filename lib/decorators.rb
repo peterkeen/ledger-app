47ea0292d9e4c00b6a8709978b6bf02775798ee8
Pete Keen <peter.keen@bugsplat.info>
2013-11-25 09:37:22 -0500

    Link last bill accounts with decorator to get account names right

--- lib/decorators.rb
+++ lib/decorators.rb
@@ -8,3 +8,13 @@ class TextColorDecorator
     cell
   end
 end
+
+class BillLinkDecorator
+  def decorate(cell, row)
+    account = cell.text
+    url = "/reports/register?account=#{account}&year=#{row[1].value.to_s}"
+    link_text = account.gsub('Expenses:', '').gsub('Liabilities:', '')
+    cell.text = "<a href=\"#{url}\">#{link_text}</a>"
+    cell
+  end
+end
