5df64ae694921ca15a441c7bc5ceb323a27143be
Pete Keen <pete@petekeen.net>
2015-11-24 09:27:37 -0500

    bump

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,4 +1,4 @@
 module Constants
-  CURRENT_ACCOUNTS = '(Assets:Schwab:Checking|Assets:Amex|Assets:BofA|Assets:PNC)'
+  CURRENT_ACCOUNTS = '(Assets:Schwab:Checking|Assets:BofA|Assets:PNC)'
   LONG_TERM_FUNDS  = 'Assets:Funds:(Travel|House|Furniture|Emergency|Reserve|Bike|Car|Medical|Stash|Insurance:Car|Insurance:Umbrella)'
 end
