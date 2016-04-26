cb6ddbf8c427ae014ea09b09652bb50c1637f60a
Pete Keen <pete@petekeen.net>
2016-04-26 18:54:15 -0400

    doh

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,5 @@
 module Constants
   CURRENT_ACCOUNTS = '(Assets:PNC|Liabilities:Chase|Assets:BofA|Assets:Schwab|Assets:Amex)'
   LONG_TERM_FUNDS  = 'Assets:Funds:(Travel|House|Furniture|Emergency|Reserve|Bike|Car|Medical|Stash|Schmoop)'
-  LIQUID_TICKERS   = 'VASIX|$'
+  LIQUID_TICKERS   = 'VASIX|\$'
 end
