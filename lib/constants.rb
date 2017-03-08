e1437b4c9bd50754d49d58da364950430d8191cc
Pete Keen <pete@petekeen.net>
2017-03-08 08:02:05 -0500

    bump

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,5 +1,5 @@
 module Constants
-  CURRENT_ACCOUNTS = '(Assets:PNC|Liabilities:Chase|Assets:BofA|Assets:Schwab|Assets:Amex|Assets:SelectAccount:HSA)'
+  CURRENT_ACCOUNTS = '(Assets:PNC|Liabilities:Chase|Assets:BofA|Assets:Schwab|Assets:Amex)'
   LONG_TERM_FUNDS  = 'Assets:Funds:(Travel|House|Furniture|Emergency|Reserve|Bike|Car|Medical|Stash|Schmoop|Abigail)'
   LONG_TERM_LIABILITIES = 'Liabilities:(Loans|PNC:HELOC)'
   LIQUID_TICKERS   = 'VASIX|\$'
