004228018ca3f123016652b9a290b622e5dfb2cd
Pete Keen <pete@petekeen.net>
2017-08-08 14:06:38 -0400

    Count HSA in liquid balance

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,8 +1,8 @@
 module Constants
-  CURRENT_ACCOUNTS = '(Assets:PNC|Liabilities:Chase|Assets:BofA|Assets:Schwab|Assets:Amex)'
+  CURRENT_ACCOUNTS = '(Assets:PNC|Liabilities:Chase|Assets:BofA|Assets:Schwab|Assets:Amex|Assets:SelectAccount)'
   LONG_TERM_FUNDS  = 'Assets:Funds:(Travel|House|Furniture|Emergency|Reserve|Bike|Car|Medical|Stash|Schmoop|Abigail)'
   LONG_TERM_LIABILITIES = 'Liabilities:(Loans|PNC:HELOC)'
-  LIQUID_TICKERS   = 'VASIX|\$'
+  LIQUID_TICKERS   = 'VASIX|SWTSX|\$'
 end
 
 if ENV['RACK_ENV'] != 'production'
