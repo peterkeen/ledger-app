7d49f7ff1450248f9101d22cfa78f0cf97ea6558
Pete Keen <pete@petekeen.net>
2017-08-21 12:57:17 -0400

    add AGG to liquid tickers

--- lib/constants.rb
+++ lib/constants.rb
@@ -2,7 +2,7 @@ module Constants
   CURRENT_ACCOUNTS = '(Assets:PNC|Liabilities:Chase|Assets:BofA|Assets:Schwab|Assets:Amex|Assets:SelectAccount)'
   LONG_TERM_FUNDS  = 'Assets:Funds:(Travel|House|Furniture|Emergency|Reserve|Bike|Car|Medical|Stash|Schmoop|Abigail)'
   LONG_TERM_LIABILITIES = 'Liabilities:(Loans|PNC:HELOC)'
-  LIQUID_TICKERS   = 'VASIX|SWTSX|\$'
+  LIQUID_TICKERS   = 'VASIX|SWTSX|AGG|\$'
 end
 
 if ENV['RACK_ENV'] != 'production'
