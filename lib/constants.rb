e39ecc8013baf7edb75fb93eb8f342714ea653aa
Pete Keen <pete@petekeen.net>
2017-10-04 07:17:24 -0400

    Modify reports for new budget scheme

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,6 +1,6 @@
 module Constants
   CURRENT_ACCOUNTS = '(Assets:PNC|Liabilities:Chase|Assets:BofA|Assets:Schwab|Assets:Amex|Assets:SelectAccount|Liabilities:BofA|Liabilities:Fidelity)'
-  LONG_TERM_FUNDS  = 'Assets:Funds:(Travel|House|Furniture|Emergency|Reserve|Bike|Car|Medical|Stash|Schmoop|Abigail)'
+  LONG_TERM_FUNDS  = '((Assets:Funds|Budget:Expenses):(Travel|House|Furniture|Emergency|Reserve|Bike|Car|Medical|Stash|Schmoop|Abigail)|Budget:Reserve)'
   LONG_TERM_LIABILITIES = 'Liabilities:(Loans|PNC:HELOC)'
   LIQUID_TICKERS   = 'VASIX|SWTSX|AGG|\$'
 end
